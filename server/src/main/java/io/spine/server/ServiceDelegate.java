a690c284aa04f8af09767fd524f2adf262fa2e2c
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-11-13 15:58:19 +0000

    Add missing article

--- server/src/main/java/io/spine/server/ServiceDelegate.java
+++ server/src/main/java/io/spine/server/ServiceDelegate.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2022, TeamDev. All rights reserved.
+ * Copyright 2025, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -136,7 +136,7 @@ abstract class ServiceDelegate<T, R> implements WithLogging {
     }
 
     /**
-     * Handles the case of internal message enclosed into the specified request.
+     * Handles the case of an internal message enclosed into the specified request.
      *
      * <p>Default implementation reports {@link UnpublishedLanguageException} to
      * the given observer.
