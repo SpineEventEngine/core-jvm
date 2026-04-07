d094426979c2476ea8c2c1d5187e8405cdca705f
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-04-07 20:48:03 +0100

    Update `config`

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -65,7 +65,12 @@ val jacksonVersion = "2.18.3"
  */
 val googleAuthToolVersion = "2.1.5"
 
-val licenseReportVersion = "2.7"
+/**
+ * Generates reports about the licenses of the dependencies for a Gradle project.
+ *
+ * https://github.com/jk1/Gradle-License-Report
+ */
+val licenseReportVersion = "3.1.2"
 
 val grGitVersion = "4.1.1"
 
@@ -141,6 +146,20 @@ val koverVersion = "0.9.1"
  */
 val shadowVersion = "9.2.2"
 
+/**
+ * The version of JUnit used to test the build scripts.
+ *
+ * @see [io.spine.dependency.test.JUnit]
+ */
+val junitVersion = "6.0.3"
+
+/**
+ * The version of Kotest used to test the build scripts.
+ *
+ * @see [io.spine.dependency.test.Kotest]
+ */
+val kotestVersion = "6.1.10"
+
 configurations.all {
     resolutionStrategy {
         force(
@@ -192,6 +211,15 @@ dependencies {
     ).forEach {
         implementation(it)
     }
+
+    testImplementation(platform("org.junit:junit-bom:$junitVersion"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
 
 dependOnBuildSrcJar()
