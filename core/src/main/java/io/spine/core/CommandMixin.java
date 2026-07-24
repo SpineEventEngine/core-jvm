c9fea9b8927bbe3f884042af973413b9d9d464a5
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-07-24 15:44:01 +0100

    Refactor `readValue` method in `CommandMixin` to use enhanced `switch`

--- core/src/main/java/io/spine/core/CommandMixin.java
+++ core/src/main/java/io/spine/core/CommandMixin.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2022, TeamDev. All rights reserved.
+ * Copyright 2026, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -108,17 +108,12 @@ interface CommandMixin
     @Internal
     @Override
     default Object readValue(Descriptors.FieldDescriptor field) {
-        switch (field.getIndex()) {
-            case 0:
-                return getId();
-            case 1:
-                return getMessage();
-            case 2:
-                return getContext();
-            case 3:
-                return getSystemProperties();
-            default:
-                return getField(field);
-        }
+        return switch (field.getIndex()) {
+            case 0 -> getId();
+            case 1 -> getMessage();
+            case 2 -> getContext();
+            case 3 -> getSystemProperties();
+            default -> getField(field);
+        };
     }
 }
